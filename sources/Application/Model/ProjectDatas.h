f4ed121ed9a59733183cae3bf69e1b06d6c50bb2
Simon Lees <sflees@suse.de>
2025-12-28 23:01:04 +1030

    Disable non implemented midi modes

--- sources/Application/Model/ProjectDatas.h
+++ sources/Application/Model/ProjectDatas.h
@@ -1,3 +1,5 @@
 char *softclipStates[] = {"Bypass", "Subtle", "Medium", "Heavy", "Insane"};
 char *softclipGainStates[] = {"[unity]", "[boost]"};
-char *midiSyncStates[] = {"off", "transport only", "tempo", "full"};
+// tempo and full are not implemented yet
+// char *midiSyncStates[] = {"off", "transport only", "tempo", "full"};
+char *midiSyncStates[] = {"off", "transport only"};
